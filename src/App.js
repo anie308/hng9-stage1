a7f617ae7ed470aa6bdfcbe84fc1793e81e0ddb1
anie308 <aniebietabasiobo@gmail.com>
2022-10-30 21:56:53 +0100

    project initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App w-full flex flex-col items-center justify-center h-full">
+      {/* avatar section */}
+      <div>
+        <div className="mt-[70px] h-[120px] w-[120px] bg-green-200 rounded-full">
+          <img src="" alt="" id="profile_img" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
